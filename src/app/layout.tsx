a3c36bf58cbd99b629b8dea59620eaa7193166e1
hoon95 <66771509+hoon95@users.noreply.github.com>
2025-02-16 02:06:11 +0900

    🍱 favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "To-Do List",
+  title: "칸반 보드",
   description: "업무의 효율을 증진시키기 위한 칸반 형태의 To-Do List",
 };
 
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="stylesheet" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
