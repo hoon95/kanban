d4ff9033b3e8f266e3e07e40b16001d7c091daa2
hoon95 <66771509+hoon95@users.noreply.github.com>
2025-03-16 20:56:48 +0900

    react scan 활용해보기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import { scan } from "react-scan";
 
-export const metadata: Metadata = {
-  title: "칸반 보드",
-  description: "업무의 효율을 증진시키기 위한 칸반 형태의 To-Do List",
-};
+if (process.env.NODE_ENV === "development") {
+  scan({
+    enabled: true,
+    log: true,
+  });
+}
+
+// export const metadata: Metadata = {
+//   title: "칸반 보드",
+//   description: "업무의 효율을 증진시키기 위한 칸반 형태의 To-Do List",
+// };
 
 export default function RootLayout({
   children,
@@ -15,6 +25,7 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />
       </head>
       <body>{children}</body>
     </html>
